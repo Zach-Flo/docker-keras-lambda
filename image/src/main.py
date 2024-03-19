150efd8d4ff010600870a9afb75303064060237c
Zach Florian <zachary.florian@uconn.edu>
2024-03-19 16:39:43 -0400

    versions

--- image/src/main.py
+++ image/src/main.py
@@ -1,5 +1,19 @@
 import numpy as np
-import keras
+import tensorflow as tf
 
 def handler(event, context):
-    return {"statusCode": 200, "body": "Hello World keras"}
+    
+    model_dir = 'saved_model'
+    load_model = tf.keras.models.load_model(model_dir)
+
+    sample_data = gen_input_data()
+    load_model.predict(sample_data)
+
+    return {"statusCode": 200, "body": tf.__version__}
+
+def gen_input_data(shape1 = (1,5),shape2 = (1,24,77)):
+    input1 = np.random.randint(low =0,high = 100,size = shape1)
+    input2 = np.random.randint(low = 0,high = 100, size = shape2)
+    inputs = [input1,input2]
+    return inputs
+    
