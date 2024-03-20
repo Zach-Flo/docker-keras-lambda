578605ff554a575a25ed15ba79da8fb5def71641
Zach Florian <zachary.florian@uconn.edu>
2024-03-19 22:53:05 -0400

    Revert "mostly working"
    This reverts commit c168b1aadf60a4bdd1e2f410fbf5cb3a91be2d71.
    
    not necessary

--- image/src/main.py
+++ image/src/main.py
@@ -1,22 +1,15 @@
 import numpy as np
 import tensorflow as tf
-import json
 
 def handler(event, context):
     
     model_dir = 'saved_model'
-
-    # Define load options with experimental_io_device
-    load_options = tf.saved_model.LoadOptions(experimental_io_device='/job:localhost')
-
-
-    load_model = tf.keras.models.load_model(model_dir, options=load_options)
+    load_model = tf.keras.models.load_model(model_dir)
 
     sample_data = gen_input_data()
     load_model.predict(sample_data)
-    list = sample_data.tolist()
-    data = json.dumps(list)
-    return {"statusCode": 200, "body": data}
+
+    return {"statusCode": 200, "body": tf.__version__}
 
 def gen_input_data(shape1 = (1,5),shape2 = (1,24,77)):
     input1 = np.random.randint(low =0,high = 100,size = shape1)
